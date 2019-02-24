5b8c993f6e1e1d17c70743b088c2f96b202cc2f2
Alexandros Nafas <alexandros.nafas@gmail.com>
2019-02-24 21:59:45 +0200

    Add firebase once

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,19 +3,49 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 public class Hello extends CordovaPlugin {
 
     @Override
+    protected void pluginInitialize() {
+        Log.d(TAG, "Starting Firebase-native plugin");
+        this.database = FirebaseDatabase.getInstance();
+    }
+
+    @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        if ("great".equals(action)) {
 
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
 
             return true;
+
+        } else if ("once".equals(action)) {
+
+            String ref = data.getString(0);
+            Log.d(TAG, "Reading from ref: " + ref);
+            database.getReference(ref).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String data = dataSnapshot.getValue(String.class);
+                    Log.d(TAG, "Got value from DB: " + data);
+                    callbackContext.success(data);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.d(TAG, "Error from DB");
+                    callbackContext.error(error.getCode());
+                }
+            });
 
         } else {
 
--- www/hello.js
+++ www/hello.js
@@ -3,5 +3,8 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    },
+    once: function(ref, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "once", [ref]);
     }
 };
