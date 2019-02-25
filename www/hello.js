57f8585fad554b7b3bb518834f61692535aba8ea
Alexandros Nafas <alexandros.nafas@gmail.com>
2019-02-25 21:48:36 +0200

    Test

--- www/hello.js
+++ www/hello.js
@@ -4,8 +4,10 @@ module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
     },
-    once: function(ref, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "once", [ref]);
+    once: function(ref) {
+        return new Promise(function(resolve, reject) {
+            cordova.exec(resolve, reject, "Hello", "once", [ref]);
+        });
     },
     test: function() {
         return new Promise(function(resolve, reject) {
