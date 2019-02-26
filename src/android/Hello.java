01b324b58e2b1cf9273b7d12b04df6263c0bf7aa
Alexandros Nafas <alexandros.nafas@gmail.com>
2019-02-26 21:34:09 +0200

    Test

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -11,6 +11,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.gson.Gson;
 
 import java.util.List;
 import java.util.Map;
