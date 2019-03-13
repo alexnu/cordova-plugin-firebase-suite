8f246c223d2800b52c26b986dd28913e888aaff7
Alexandros Nafas <alexandros.nafas@gmail.com>
2019-03-13 10:40:24 +0200

    Fix

--- src/android/FirebaseNativePlugin.java
+++ src/android/FirebaseNativePlugin.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.database.FirebaseDatabase;
