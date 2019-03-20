276f81081906e5c939a49eb43fe61417f2bfe5f4
Alexandros Nafas <alexandros.nafas@gmail.com>
2019-03-20 21:47:40 +0200

    Fix

---
+++ src/android/FirebaseAuthPlugin.java
@@ -0,0 +1,51 @@
+package com.alexnu.plugin;
+
+import android.util.Log;
+
+import org.apache.cordova.*;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.support.annotation.NonNull;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.lang.reflect.Type;
+
+
+public class FirebaseNativePlugin extends CordovaPlugin {
+
+    private static final String TAG = "FirebaseAuth";
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void pluginInitialize() {
+        Log.d(TAG, "Starting FirebaseAuth plugin");
+
+        this.auth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        Log.d(TAG, "Got new action " + action);
+
+    }
+
+}
