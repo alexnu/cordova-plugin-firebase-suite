bcf7e0207fcfc3eaeb7a84b2e1d8b6cd373aa451
Alexandros Nafas <alexandros.nafas@gmail.com>
2019-11-06 08:54:30 +0200

    Add facebook app delegate

---
+++ src/ios/auth/AppDelegate+FirebaseFacebookAuthPlugin.m
@@ -0,0 +1,47 @@
+#import "AppDelegate+FirebaseFacebookAuthPlugin.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+@implementation AppDelegate (FirebaseFacebookAuthPlugin)
+
++ (void)load {
+
+    swizzleMethod([AppDelegate class],
+            @selector(application:openURL:options:),
+            @selector(identity_application:openURL:options:));
+}
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+    didFinishLaunchingWithOptions:launchOptions];
+  // Add any custom logic here.
+  return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+
+  BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
+    openURL:url
+    sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
+    annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
+  ];
+  // Add any custom logic here.
+  return handled;
+}
+
+static void swizzleMethod(Class class, SEL destinationSelector, SEL sourceSelector) {
+    Method destinationMethod = class_getInstanceMethod(class, destinationSelector);
+    Method sourceMethod = class_getInstanceMethod(class, sourceSelector);
+
+    // If the method doesn't exist, add it.  If it does exist, replace it with the given implementation.
+    if (class_addMethod(class, destinationSelector, method_getImplementation(sourceMethod), method_getTypeEncoding(sourceMethod))) {
+        class_replaceMethod(class, destinationSelector, method_getImplementation(destinationMethod), method_getTypeEncoding(destinationMethod));
+    } else {
+        method_exchangeImplementations(destinationMethod, sourceMethod);
+    }
+}
+
+@end
